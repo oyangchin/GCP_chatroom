c772f21a99bc196845fb007aa33c06b8b43e1f6a
oyangchin <woothy@gmail.com>
2018-12-03 21:00:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// Copyright 2018, Google LLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+
+const http = require('http').Server(express);
+const io = require('socket.io')(http);
+const app = express();
+
+// [START hello_world]
+// Say hello!
+app.get('/', (req, res) => {
+  // res.status(200).send('Hello, Ning! 今天約會 gogogogo<3');
+  res.sendFile(__dirname + '/index.html');
+});
+// [END hello_world]
+
+var user_count = 0;
+
+//當新的使用者連接進來的時候
+io.on('connection', function(socket){
+
+	//新user
+	socket.on('add user',function(msg){
+		socket.username = msg;
+		console.log("new user:"+msg+" logged.");
+		io.emit('add user',{
+			username: socket.username
+		});
+	});
+
+	//監聽新訊息事件
+	socket.on('chat message', function(msg){
+
+		console.log(socket.username+":"+msg);
+
+  		//發佈新訊息
+		io.emit('chat message', {
+			username:socket.username,
+			msg:msg
+		});
+	});
+
+	//left
+	socket.on('disconnect',function(){
+		console.log(socket.username+" left.");
+		io.emit('user left',{
+			username:socket.username
+		});
+	});
+
+
+});
+
+
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
