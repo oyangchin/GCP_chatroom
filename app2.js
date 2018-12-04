fde83a5e9274c1279935b966c134921112c8a09f
oyangchin <woothy@gmail.com>
2018-12-04 11:54:19 +0800

    change app2.js

--- app2.js
+++ app2.js
@@ -14,9 +14,10 @@
 'use strict';
 
 const app = require('express')();
+// const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-// const app = express();
+
 
 // [START hello_world]
 // Say hello!
